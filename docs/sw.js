93cae202824a1406377b9deb2b70951d95dcdcbf
gue1971 <gue1971@gmail.com>
2026-06-04 15:06:42 +0900

    Refresh service worker cache strategy

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "photo-paddock-v4";
+const CACHE_NAME = "photo-paddock-v5";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -34,15 +34,12 @@ self.addEventListener("fetch", (event) => {
   if (url.origin !== location.origin) return;
 
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      const fetched = fetch(event.request).then((response) => {
-        if (response.ok) {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-        }
-        return response;
-      }).catch(() => cached);
-      return cached || fetched;
-    })
+    fetch(event.request).then((response) => {
+      if (response.ok) {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+      }
+      return response;
+    }).catch(() => caches.match(event.request))
   );
 });
